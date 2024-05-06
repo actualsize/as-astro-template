94400428f1b933414c37e468f1569346b0240fd1
Kevin <kevinstewartmercurio@gmail.com>
2024-05-06 11:10:39 -0400

    feat: adding sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -28,6 +29,13 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    sentry({
+      dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
+      sourceMapsUploadOptions: {
+        project: "example-project",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
   ],
   redirects: {
     "/admin/[...slug]": "/admin",
